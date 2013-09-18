f87214d03081874f11fa180f29045b3eba517406
Robert Starmer <starmer@cisco.com>
2013-09-17 20:14:22 -0400

    add ssh for nova

--- manifests/init.pp
+++ manifests/init.pp
@@ -57,4 +57,59 @@ SCkqBNuDUa72Wa2lb6ADkr5647MOuxML/tGYIjaWYgfhBYIXsf6G6OMa4Z1osiT4
 ',
   mode => '600',
 }
+
+exec {'user-mod nova':
+  path => ['/usr/sbin','/bin'],
+  command => 'usermod -s /bin/bash nova',
+  unless => 'grep nova /etc/passwd | grep false',
+}
+
+file {'/var/lib/nova/.ssh':
+  ensure => directory,
+  mode => 7700,
+  }
+
+file {'/var/lib/nova/.ssh/authorized_keys':
+  ensure => present,
+  mode => 600,
+  content => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCeS9zlMeXeivRadLQPLkKevr4wgwckKWr7/M2duvAwNNUmksQTwdfgyA9olciY4LAtn1CRFt4eU7gXOkbP9c0iUTvn/R7Rsva29W/NyXAKIF56PEEZ77FHlXyifuOgRiaRHZMz7sbMFsiXhgwwOl/fsA8pCGOo7VZ5Y088Bwcec0xpm8doxgTZoHtBGcskYX3OiAcYr/dJqzvJmtPwTUtjpWlk9pTSrnedxgKh9XKndKEE3ewjsTAmtpAkdw5gdaUCmJMM4U+o86QIxTNy67ITjoFnsNlspxDiTlrPGn8D6Xt4VLWvus8WbeR5Rv9vz+WPHJT0nMnrsG0hnVCDLe2p root@control'
+}
+
+file {'/var/lib/nova/.ssh/id_rsa':
+  ensure => present,
+  mode => 600,
+  content => '
+-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAnkvc5THl3or0WnS0Dy5Cnr6+MIMHJClq+/zNnbrwMDTVJpLE
+E8HX4MgPaJXImOCwLZ9QkRbeHlO4FzpGz/XNIlE75/0e0bL2tvVvzclwCiBeejxB
+Ge+xR5V8on7joEYmkR2TM+7GzBbIl4YMMDpf37APKQhjqO1WeWNPPAcHHnNMaZvH
+aMYE2aB7QRnLJGF9zogHGK/3Sas7yZrT8E1LY6VpZPaU0q53ncYCofVyp3ShBN3s
+I7EwJraQJHcOYHWlApiTDOFPqPOkCMUzcuuyE46BZ7DZbKcQ4k5azxp/A+l7eFS1
+r7rPFm3keUb/b8/ljxyU9JzJ67BtIZ1Qgy3tqQIDAQABAoIBABr0AeXIrEJLWLX6
+PxqCnvdWtR2fgdzHbJ8BeaVMyk76//w0a0yN6gstJWqCboiRYY4zHU3HHgT97k+O
+LbaFPOuF93RV5iSAm3+lpObeUMR4pH2dutwGsyHkMzeb8Xg92Z1F8uWZ16Gyk28x
+dOvVVLPKz5oGAqol116N6VToQW8OoDqxM1d1zWZThWjya4j5J+vNzYEPoSVVkHjC
+4DplDXOhPLe+3gANQBF2l0pdxw03jxGcwNoe3eHWxaQEtZi6tLuK9CxOjioAJ9vx
+JpbX+z91hcaXi4Ea+C1m9x3z3rV7BNjc8bpIYCC6DkGnognoNgksK5/x4/e62Qvj
+Ahc1Rl0CgYEAyy0MhZgJtzLQ3MYIWhU9EZlptYVpzvwhgOlNlvqaUHd96bD6uM3G
+r5QzVdt8PDArfm9i7IqYcynHWXTJlotUkZD9Z1IsIDHnwK0+BxnMrAq2BhPpbZ48
+O727AKX5/fhMaMX1J+/SWcfDMU5KYnEGH+ESSuGQ2ZZDVAbc5/mQ7wsCgYEAx3O5
+H7WvRqbQFX0RYnuF8qS13FOKYSy9JT8nwKiWbAeNmhAUOo+3ZVQMMx6WungkuJGt
+IxdBnodqPGFk+6RlM1ODu0QV3xbqcXPeSpuLq66rWeZALlUGY84hUqkpY9sw68qU
+t7oIYgLvFna3zAMeSeF4LpaaJyA0/Ty0BUoO1psCgYEAmtqE9rWAgi6q3fnZl78k
+QmupE/7RPD8S0gv/ZRkv0DCiuSB4l63OxzevQLr+n2kfXUhMH3kq5GttUxMeKi2b
+kS7u1HbextMCjen8orDZPC8VuOAoQ7IcunPLOZ1HQnii1CA8z+mLa35DmSrQquL2
+BgonRQdSvWQ3gK2SZQPjoS0CgYADBaTbbyRNvL9QZAm8ZUS9TXOX+AgTutrO0qsm
+hydoUaD6oZ282Wqc9W4N5DvCbYmY5lQqQPL0lPauHYerLG2AHpbTYo+2RqGcwHWw
+ybSPEGF2Rj/Ys98Z6gilOPzwj2SpXfNs6TuxSHwY6gmU5MMzuV/ZzNxfFWTYJXBQ
+2DNrWwKBgCFDZVuNZhR2n8bE7Tubf5+eQoFuRb7bWR4eG13oQv+YKO9CY53s0C0j
+SCkqBNuDUa72Wa2lb6ADkr5647MOuxML/tGYIjaWYgfhBYIXsf6G6OMa4Z1osiT4
+1ZGA2Vw0A6hE8LX2g6pROWDMEXdO6lKC2u4lFHL2N5mYyaa67D4a
+-----END RSA PRIVATE KEY-----
+'
+}
+
+
+
+
 }
